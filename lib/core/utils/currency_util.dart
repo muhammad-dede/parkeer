0b57a73a535183bdc5f432c70bd9bfb357567787
Muhammad Dede <m.dedenuraen@gmail.com>
2026-07-02 21:22:16 +0700

    feat:currency util

---
+++ lib/core/utils/currency_util.dart
@@ -0,0 +1,15 @@
+import 'package:intl/intl.dart';
+
+class CurrencyUtil {
+  CurrencyUtil._();
+
+  static final NumberFormat _formatter = NumberFormat.currency(
+    locale: 'id_ID',
+    symbol: 'Rp ',
+    decimalDigits: 0,
+  );
+
+  static String format(num value) {
+    return _formatter.format(value);
+  }
+}
